7465939c12ceb4a8df869d0b5b2e2e957567c78a
AlexAaltonen <alex.aaltonen99@gmail.com>
2025-08-27 16:36:54 +0300

    ok

--- mod-03/teht.4.py
+++ mod-03/teht.4.py
@@ -3,7 +3,7 @@ vuosi = int(input("kirjoita vuosi luku: "))
 if vuosi % 4 != 0:
     print("vuosi ei ole karkausvuosi")
 
-elif vuosi % 100 != 0 and vuosi % 400 != 0:
+elif vuosi % 100 == 0 and vuosi % 400 != 0:
     print("vuosi ei ole karkausvuosi")
 
 else:
