ac9d2bdad8106ded4431fdf0ee5c54e41b53232b
AlexAaltonen <alex.aaltonen99@gmail.com>
2025-09-09 11:13:02 +0300

    ok

--- mod-06/teht. 5.py
+++ mod-06/teht. 5.py
@@ -1,10 +1,14 @@
 def parillinen(lista):
-    return [luku for luku in lista if luku % 2 == 0]
+    parillinen_lista = []
+    for luku in lista:
+        if luku % 2 == 0:
+            parillinen_lista.append(luku)
+    return parillinen_lista
 
 
 
 alkuperainen_lista=[1, 2, 3 ,4, 5, 6]
 
-print(parillinen(alkuperainen_lista))
+parillinen_lista = parillinen(alkuperainen_lista)
 
-
+print(parillinen_lista)
